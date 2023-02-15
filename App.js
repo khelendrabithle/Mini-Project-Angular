917c1fcff085d97b4b965b239affb75c97186067
Khelendra bithle <118499886+khelendrabithle@users.noreply.github.com>
2023-02-15 07:40:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import{Fragment} from 'react';
+import { Header } from './components/Layout/Header';
+
+function app()
+{
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+export default app;
